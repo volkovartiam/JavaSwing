629213aafa6daa0bec6ed0b215c182446b1ae460
Artem Volkov <volkov_artiam@mail.ru>
2021-11-01 19:35:04 +0200

    Simple centering frame with buttons beep and close

--- src/Simple.java
+++ src/Simple.java
@@ -1,13 +1,16 @@
-
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * Example from book The Java Swing tutorial crated by dovari.sudheerkiran@gmail.com 
- * page6.
- * Centering on the screen
+ * page10.
+ * Centering on the screen with buttons beep(make sounds) and close(exit frame)
  *
  * @author Volkov Artem
  *
@@ -15,17 +18,44 @@ import javax.swing.JFrame;
 */
 public class Simple extends JFrame {
   
+
+  private Toolkit toolkit;
+
   /** 
   * ...Constructor for sets parameters of JFrame...
   */
   public Simple() {
+    setTitle("Buttons");
     setSize(400, 300);
-    setTitle("Simple ");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     
-    Toolkit toolkit = getToolkit() ;
+    toolkit = getToolkit();
     Dimension size = toolkit.getScreenSize();
-    setLocation(size.width/2 - getWidth()/2 ,  size.height/2 - getHeight()/2   );
+    setLocation(size.width/2 - getWidth()/2,  size.height/2 - getHeight()/2);
+    
+    JPanel panel = new JPanel();
+    getContentPane().add(panel);
+    
+    panel.setLayout(null);
+    
+    JButton beep = new JButton("Beep");
+    beep.setBounds(80, 60, 80, 40);
+    beep.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+            toolkit.beep();
+        }
+    });
+    
+    JButton close = new JButton("Close");
+    close.setBounds(200, 60, 80, 40);
+    close.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+            System.exit(0);
+        }
+    });
+    
+    panel.add(beep);
+    panel.add(close);
   }
   
   /** 
