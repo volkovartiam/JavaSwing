d103e09bbd545d1961af007727fb679df9eaba21
Artem Volkov <volkov_artiam@mail.ru>
2021-12-01 19:45:53 +0200

    GUI with show message

--- src/FrameDialog.java
+++ src/FrameDialog.java
@@ -1,8 +1,13 @@
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 /**
  * Frame for demonstrate dialog message 
@@ -14,6 +19,8 @@ import javax.swing.JMenuItem;
 */
 public class FrameDialog extends JFrame  {
 
+  JPanel panel = new JPanel();
+  
   /** 
   * ...Constructor for sets parameters of FrameDialog...
   */
@@ -34,12 +41,45 @@ public class FrameDialog extends JFrame  {
         dialog.setVisible(true);
       }
     });
+    
+    panel.setLayout(new GridLayout(2, 2));
+    
+    JButton btnError = new JButton("Error");
+    JButton btnWarning = new JButton("Warning");
+
+    btnError.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent event) {
+        JOptionPane.showMessageDialog(panel, "Could not open file", 
+            "Error", JOptionPane.ERROR_MESSAGE);
+      }
+    });
+    btnWarning.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent event) {
+        JOptionPane.showMessageDialog(panel, "A deprecated call", 
+            "Warning", JOptionPane.WARNING_MESSAGE);
+      }
+    });
+
+    JButton btnQuestion = new JButton("Question");
+    JButton btnInfo = new JButton("Information");
+
+    btnQuestion.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent event) {
+        JOptionPane.showMessageDialog(panel, "Are you sure  to quit?", 
+            "Question", JOptionPane.QUESTION_MESSAGE);
+      }
+    });
+    btnInfo.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent event) {
+        JOptionPane.showMessageDialog(panel, "Download completed", 
+            "Message", JOptionPane.INFORMATION_MESSAGE);
+      }
+    });
+    
+    panel.add(btnError);
+    panel.add(btnWarning);
+    panel.add(btnQuestion);
+    panel.add(btnInfo);
+    add(panel);
   }
 }  
-  
-
-  
-
-
-
-
